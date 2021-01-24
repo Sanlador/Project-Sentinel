9e2e895af63f541db8230d14cec4e8cfdf7fbd59
Sanlador <cunardr@oregonstate.edu>
2021-01-23 20:15:23 -0800

    Added hand-differentiation
    Controller input will now differentiate between left and right-handed controllers, provided they are of the same type and contain all buttons specified.

--- Project Sentinel/Assets/_Scripts/ControllerScripts/Primary/PrimaryButtonWatcher.cs
+++ Project Sentinel/Assets/_Scripts/ControllerScripts/Primary/PrimaryButtonWatcher.cs
@@ -8,24 +8,36 @@ public class ButtonEvent : UnityEvent<bool> { }
 
 public class PrimaryButtonWatcher : MonoBehaviour
 {
-    public List<ButtonEvent> buttonEvents = new List<ButtonEvent>();
-    private List<bool> previousButtonStates = new List<bool>();
+    public List<ButtonEvent> buttonEventsLeft = new List<ButtonEvent>();
+    public List<ButtonEvent> buttonEventsRight = new List<ButtonEvent>();
+    private List<bool> previousButtonStatesLeft = new List<bool>();
+    private List<bool> previousButtonStatesRight = new List<bool>();
     private List<InputDevice> controllers;
 
-    InputFeatureUsage<bool>[] buttons = { CommonUsages.primaryButton, CommonUsages.triggerButton,
-     CommonUsages.gripButton, CommonUsages.menuButton};
+    //list of buttons to be used in application; must be manually updated
+    InputFeatureUsage<bool>[] boolButtons = 
+    { 
+        CommonUsages.primaryButton,
+        CommonUsages.triggerButton, 
+        CommonUsages.gripButton, 
+        CommonUsages.menuButton
+    };
 
     private void Awake()
     {
-        for (int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < boolButtons.Length; i++)
         {
-            buttonEvents.Add(new ButtonEvent());
-            previousButtonStates.Add(false);
+            //must keep separate lists for left and right controllers to allow for differentiation
+            buttonEventsLeft.Add(new ButtonEvent());
+            buttonEventsRight.Add(new ButtonEvent());
+            previousButtonStatesLeft.Add(false);
+            previousButtonStatesRight.Add(false);
         }
 
         controllers = new List<InputDevice>();
     }
 
+    //checks for abailable controllers
     void OnEnable()
     {
         List<InputDevice> allDevices = new List<InputDevice>();
@@ -37,6 +49,7 @@ public class PrimaryButtonWatcher : MonoBehaviour
         InputDevices.deviceDisconnected += InputDevices_deviceDisconnected;
     }
 
+    //triggers on controller disabling; removes from list
     private void OnDisable()
     {
         InputDevices.deviceConnected -= InputDevices_deviceConnected;
@@ -59,25 +72,51 @@ public class PrimaryButtonWatcher : MonoBehaviour
             controllers.Remove(device);
     }
 
+    private void getButtonStates(List<bool> previousButtonStates, List<ButtonEvent> buttonEvents, List<InputDevice> c, int itr)
+    {
+        bool tempState = false;
+        foreach (var device in c)
+        {
+            bool currentButtonState = false;
+            tempState = device.TryGetFeatureValue(boolButtons[itr], out currentButtonState) // did get a value
+                        && currentButtonState // the value we got
+                        || currentButtonState; // cumulative result from other controllers
+        }
+
+        if (tempState != previousButtonStates[itr]) // Button state changed since last frame
+        {
+            buttonEvents[itr].Invoke(tempState);
+            previousButtonStates[itr] = tempState;
+        }
+    }
+
     void Update()
     {
-        
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            bool tempState = false;
-            foreach (var device in controllers)
-            {
-                bool currentButtonState = false;
-                tempState = device.TryGetFeatureValue(buttons[i], out currentButtonState) // did get a value
-                            && currentButtonState // the value we got
-                            || currentButtonState; // cumulative result from other controllers
-            }
+        //discount non-VR controllers
 
-            if (tempState != previousButtonStates[i]) // Button state changed since last frame
-            {
-                buttonEvents[i].Invoke(tempState);
-                previousButtonStates[i] = tempState;
-            }
+        //set filtering parameters
+        var leftCharacteristics =
+            UnityEngine.XR.InputDeviceCharacteristics.HeldInHand |
+            UnityEngine.XR.InputDeviceCharacteristics.Left |
+            UnityEngine.XR.InputDeviceCharacteristics.Controller;
+        var rightCharacteristics =
+            UnityEngine.XR.InputDeviceCharacteristics.HeldInHand |
+            UnityEngine.XR.InputDeviceCharacteristics.Right |
+            UnityEngine.XR.InputDeviceCharacteristics.Controller;
+
+
+        //split into lists based on left and right-handed controllers
+        List<InputDevice> leftHandedControllers = new List<UnityEngine.XR.InputDevice>();
+        List<InputDevice> rightHandedControllers = new List<UnityEngine.XR.InputDevice>();
+
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(leftCharacteristics, leftHandedControllers);
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(rightCharacteristics, rightHandedControllers);
+
+        //checks the state of each button from button list
+        for (int i = 0; i < boolButtons.Length; i++)
+        {
+            getButtonStates(previousButtonStatesLeft, buttonEventsLeft, leftHandedControllers, i);
+            getButtonStates(previousButtonStatesRight, buttonEventsRight, rightHandedControllers, i);
         }
         
     }
--- Project Sentinel/Assets/_Scripts/ControllerScripts/Primary/PrimaryReactor.cs
+++ Project Sentinel/Assets/_Scripts/ControllerScripts/Primary/PrimaryReactor.cs
@@ -11,14 +11,17 @@ public class PrimaryReactor : MonoBehaviour
     void Start()
     {
         //initiate listener from watcher class
-        watcher.buttonEvents[0].AddListener(onPrimaryButtonEvent);
-        watcher.buttonEvents[1].AddListener(onSecondaryButtonEvent);
-        watcher.buttonEvents[2].AddListener(onGripEvent);
+        watcher.buttonEventsLeft[0].AddListener(onPrimaryButtonEventLeft);
+        watcher.buttonEventsLeft[1].AddListener(onSecondaryButtonEventLeft);
+        watcher.buttonEventsLeft[2].AddListener(onGripEventLeft);
+        watcher.buttonEventsRight[0].AddListener(onPrimaryButtonEventRight);
+        watcher.buttonEventsRight[1].AddListener(onSecondaryButtonEventRight);
+        watcher.buttonEventsRight[2].AddListener(onGripEventRight);
     }
 
-    public void onPrimaryButtonEvent(bool pressed)
+    public void onPrimaryButtonEventLeft(bool pressed)
     {
-        Debug.Log("Primary button pressed");
+        Debug.Log("Left primary button pressed");
         IsPressed = pressed;
 
         //coroutine example code
@@ -32,13 +35,39 @@ public class PrimaryReactor : MonoBehaviour
             rotator = StartCoroutine(AnimateRotation(this.transform.rotation, offRotation));*/
     }
 
-    public void onSecondaryButtonEvent(bool pressed)
+    public void onSecondaryButtonEventLeft(bool pressed)
     {
-        Debug.Log("Trigger pressed");
+        Debug.Log("Left trigger pressed");
     }
 
-    public void onGripEvent(bool pressed)
+    public void onGripEventLeft(bool pressed)
     {
-        Debug.Log("Grip pressed");
+        Debug.Log("Left grip pressed");
+    }
+
+    public void onPrimaryButtonEventRight(bool pressed)
+    {
+        Debug.Log("Right primary button pressed");
+        IsPressed = pressed;
+
+        //coroutine example code
+        /*
+         * Sets up coroutine to run asynchronously, allowing for multi-frame functions
+         * if (rotator != null)
+            StopCoroutine(rotator);
+        if (pressed)
+            rotator = StartCoroutine(AnimateRotation(this.transform.rotation, onRotation)); 
+        else
+            rotator = StartCoroutine(AnimateRotation(this.transform.rotation, offRotation));*/
+    }
+
+    public void onSecondaryButtonEventRight(bool pressed)
+    {
+        Debug.Log("Right trigger pressed");
+    }
+
+    public void onGripEventRight(bool pressed)
+    {
+        Debug.Log("Right grip pressed");
     }
 }
